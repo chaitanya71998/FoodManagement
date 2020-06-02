c30a71c2e0ad6a0209efd5a68ba089491b3eb824
Lingareddyvinitha <r151453@rguktrkv.ac.in>
2020-06-02 16:49:51 +0000

    upstream fonts

--- src/FoodManagement/utils/TimeUtils.js
+++ src/FoodManagement/utils/TimeUtils.js
@@ -4,17 +4,21 @@ import formatDistance from 'date-fns/formatDistance'
 
 // let result = formatDistance(
 
-        //   new Date('2020-06-02 19:45:00'),
-        //   new Date(), { addSuffix: true }
-        // )
-        // console.log("result", result)
-        // let result1 = isBefore(new Date("2020-06-02 18:47:00"), new Date("2020-06-02 18:48:00"))
-        // console.log("result1", result1)
+//   new Date('2020-06-02 19:45:00'),
+//   new Date(), { addSuffix: true }
+// )
+// console.log("result", result)
+// let result1 = isBefore(new Date("2020-06-02 18:47:00"), new Date("2020-06-02 18:48:00"))
+// console.log("result1", result1)
 
 export function getTimeDistanceInWords(deadLine) {
-   return formatDistance(
+    return formatDistance(
 
-   new Date(deadLine),
-   new Date(), { addSuffix: true }
-)
+        new Date(deadLine),
+        new Date(), { addSuffix: true }
+    )
+}
+
+export function isTimeBeforeDeadLine() {
+
 }
